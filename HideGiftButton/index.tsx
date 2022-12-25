cfb5b2f4f48840ded95e577fe974499607ddbec4
slyde99 <slydentn@gmail.com>
2022-12-26 00:39:05 +0800

    [HideGiftButton] husky

--- HideGiftButton/index.tsx
+++ HideGiftButton/index.tsx
@@ -1,14 +1,12 @@
-import { Plugin } from "aliucord/entities"
 // @ts-ignore
-import { getByProps, FluxDispatcher, getByName } from "aliucord/metro";
-import { findInReactTree } from "aliucord/utils";
+import { getByName } from "aliucord/metro";
+import { Plugin } from "aliucord/entities"
 
 export default class HideGiftButton extends Plugin {
     public start() {
         // why does it need to be async lmao
         (async () => {
             const ChatInput = getByName("ChatInput");
-            console.log(ChatInput);
 
             this.patcher.before(ChatInput.default.prototype, "render", (x) => {
                 x.thisObject.props.hideGiftButton = true;
